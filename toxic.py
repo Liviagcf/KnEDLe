8418d4eb4d88e3785b7964d58638b08d679c9dae
Liviagcf <liviagcf@gmail.com>
2020-05-04 22:48:51 -0300

    Escolha do classificador e seleção dos exemplos rotulados

--- toxic.py
+++ toxic.py
@@ -4,6 +4,7 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import nltk
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
 import seaborn as sns
@@ -11,6 +12,13 @@ from sklearn.preprocessing import normalize
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.gridspec as gridspec
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.metrics import multilabel_confusion_matrix
+
+
+
 
 
 data = pd.read_csv("train.csv")
@@ -23,7 +31,7 @@ df = pd.DataFrame(data)
 *   Remocao de pontuacao.
 """
 
-def clean_text(text):
+def cleanText(text):
     text = text.lower()
     text = re.sub(r"what's", "what is ", text)
     text = re.sub(r"\'s", " ", text)
@@ -40,18 +48,67 @@ def clean_text(text):
     text = text.strip(' ')
     return text
 
-df['comment_text'] = df['comment_text'].map(lambda com : clean_text(com))
+
+"""Funcao para plotar Matrix de confusao:"""
+
+def plotConfusionMatrix(y_true, y_pred, classes, title):
+    # Calcula acuracia
+    acc = accuracy_score(y_true, y_pred)
+
+    plt.figure(figsize=(10,10))
+
+    for i in range(6):
+      # Constroi matriz de confusao
+      cm = multilabel_confusion_matrix(y_true, y_pred)[i]
+      cm = normalize(cm, axis=1, norm='l1')
+      cm_df = pd.DataFrame(cm, index = classes, columns = classes)
+      plt.subplot(3,3,i+1)
+      sns.heatmap(cm_df, annot=True, cmap="YlGnBu")
+      plt.title(title[i])
+      plt.ylabel('True label')
+      plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.show()
+
+
+
+def benchmark():
+	pass
+
+def clfTest():
+	pass
+
+
+
+bin_clf = LinearSVC(loss='l2', penalty='l2',dual=False, tol=1e-3)
+clf = OneVsRestClassifier(bin_clf, n_jobs = -1)
+
+
+
+df['comment_text'] = df['comment_text'].map(lambda com : cleanText(com))
 
 categories = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 comment = ['comment_text']
 
-dataset_serie, _ = train_test_split(df, random_state=42, test_size=0.0001, shuffle=True)
+"""Cria o dataframe coom os exemplos rotulados:
+
+*	Seleciona um exemplo para cada rotulo
+"""
+
+df_train = pd.DataFrame()
+
+for categorie in categories:
+	df_train = df_train.append( df[df[categorie]==1][0:1], ignore_index=True )
+	df.drop(index = df[df[categorie]==1][0:1].index, inplace=True)
+
+
+
+
+dataset_serie, _ =  train_test_split(df, random_state=42, test_size=0.0001, shuffle=True)
 
 dataset_serie_comment = dataset_serie.comment_text
 dataset_serie_categories = dataset_serie[categories]
 
-print(dataset_serie_comment.head(), "\n\n")
-print(dataset_serie_categories.head())
 
 """Extracao dos descritores dos comentarios:"""
 
@@ -67,25 +124,3 @@ data_train = comment_vec[0:limite_train,]
 data_test = comment_vec[limite_train:,]
 label_train = dataset_serie_categories.iloc[0:limite_train,]
 label_test = dataset_serie_categories.iloc[limite_train:,]
-
-"""Funcao para plotar Matrix de confusao:"""
-
-def plot_confusion_matrix(y_true, y_pred, classes, title):
-    # Calcula acuracia
-    acc = accuracy_score(y_true, y_pred)
-
-    plt.figure(figsize=(10,10))
-
-    for i in range(6):
-      # Constroi matriz de confusao
-      cm = multilabel_confusion_matrix(y_true, y_pred)[i]
-      cm = normalize(cm, axis=1, norm='l1')
-      cm_df = pd.DataFrame(cm, index = classes, columns = classes)
-      plt.subplot(3,3,i+1)     
-      sns.heatmap(cm_df, annot=True, cmap="YlGnBu")
-      plt.title(title[i])
-      plt.ylabel('True label')
-      plt.xlabel('Predicted label')
-    plt.tight_layout()
-    plt.show()
-
